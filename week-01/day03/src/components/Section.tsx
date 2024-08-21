ccc30d641f9b0c05d1e747441ee01ff05bf1765b
Saeyoung Choi <75560587+effysogood@users.noreply.github.com>
2024-08-21 17:27:23 +0900

    feat: section 컴포넌트 props 전달

--- week-01/day03/src/components/Section.tsx
+++ week-01/day03/src/components/Section.tsx
@@ -1,14 +1,18 @@
-interface sectionProps {
+interface SectionProps {
   id: number;
   title: string;
   desc: string;
+  onClickHandler: (title: string) => void;
 }
 
-function Section({ id, title, desc }: sectionProps) {
+function Section({ id, title, desc, onClickHandler }: SectionProps) {
   return (
     <section id={`section ${id}`}>
       <h2>{title}</h2>
       <p>{desc}</p>
+      <button type='button' onClick={() => onClickHandler(title)}>
+        See More
+      </button>
     </section>
   );
 }
