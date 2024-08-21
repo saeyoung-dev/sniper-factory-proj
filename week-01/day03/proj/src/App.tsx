f42a958cbcecffec239901f1bdc3bd6ab19b7df6
Saeyoung Choi <effysogood@gmail.com>
2024-08-21 11:52:58 +0900

    chore: tailwindcss 세팅

--- week-01/day03/proj/src/App.tsx
+++ week-01/day03/proj/src/App.tsx
@@ -1,7 +1,21 @@
+import { twMerge } from 'tailwind-merge';
+
 const App = () => {
+  const isError = false;
   return (
     <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <h1
+        className={twMerge(
+          'text-3xl, font-bold, text-green-500',
+          isError && 'text-rose-400'
+        )}
+      >
+        Hello world!
+      </h1>
+      <form>
+        <input type='text' />
+        <button>Click Here</button>
+      </form>
     </>
   );
 };
--- week-01/day03/proj/tailwind.config.js
+++ week-01/day03/proj/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
   future: {
     hoverOnlyWhenSupported: true,
   },
