53506fb26d9d3cd62c4467e16b4df4bed14bb271
Saeyoung Choi <effysogood@gmail.com>
2024-08-27 12:16:43 +0900

    feat: useMemo 예제

--- week-02/day-06/src/App.tsx
+++ week-02/day-06/src/App.tsx
@@ -1,11 +1,15 @@
 // import Todo from "./components/todo-publish/Todo";
-import UseTransition from './components/react-hook/useTransition/UseTransition';
+// import UseTransition from './components/react-hook/useTransition/UseTransition';
+// import UserCallback from './components/memoization/usecallback/UseCallback';
+import UseMemo from './components/memoization/useMemo/UseMemo';
 
 export default function App() {
   return (
     <>
       {/* <Todo /> */}
-      <UseTransition />
+      {/* <UseTransition /> */}
+      {/* <UserCallback /> */}
+      <UseMemo />
     </>
   );
 }
